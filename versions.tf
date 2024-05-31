1bdce2d4b13472d34cdbf6c86efbf8a2a562508a
kolosovpetro <kolosovp94@gmail.com>
2024-05-31 14:20:32 +0200

    format

--- versions.tf
+++ versions.tf
@@ -10,8 +10,8 @@ terraform {
 }
 
 provider "azuredevops" {
- org_service_url    = "https://dev.azure.com/PetroKolosovProjects"
- client_id          = "4d65956c-5661-494f-9e7e-c171aec0e53f"
- tenant_id          = "b40a105f-0643-4922-8e60-10fc1abf9c4b"
- client_secret_path = var.client_secret_path
+  org_service_url    = "https://dev.azure.com/PetroKolosovProjects"
+  client_id          = "4d65956c-5661-494f-9e7e-c171aec0e53f"
+  tenant_id          = "b40a105f-0643-4922-8e60-10fc1abf9c4b"
+  client_secret_path = var.client_secret_path
 }
